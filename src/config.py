8431e6c569686e8a2ed41bb78a3ba209526fc6e0
Breno Carvalho <brenos200304@gmail.com>
2024-02-08 10:11:14 -0300

    banco

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'FsjdejefweFRFWG#3452%@%@TRWWewrgwg4rtwghyettwwt254536g'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:breno19042003@brenocodesbanco.clysq3fxahpq.us-east-1.rds.amazonaws.com/brenocodesbanco'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:breno19042003@localhost/estoqueapi'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:breno19042003@brenocodesbanco.clysq3fxahpq.us-east-1.rds.amazonaws.com/brenocodesbanco'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:breno19042003@localhost/estoqueapi'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
